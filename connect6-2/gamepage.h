1f20588e86747a1e985903368f586a959fc51dde
emicod <67007537+emicod@users.noreply.github.com>
2020-10-24 03:20:33 +0000

    Add files via upload

---
+++ connect6-2/gamepage.h
@@ -0,0 +1,47 @@
+#ifndef GAMEPAGE_H
+#define GAMEPAGE_H
+
+#include <QWidget>
+#include <QString>
+#include <QPaintEvent>
+#include <QMouseEvent>
+#include <QMessageBox>
+#include <QPushButton>
+#include <QTimerEvent>
+
+class gamepage : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit gamepage(QWidget *parent = nullptr);
+
+    bool flag1, flag2;//判断是否和棋或胜利
+    int id;//定时器id，用于killtimer
+    int x, y, turn, connectv, connecth, connectl, connectr;
+    int checkerBoard[21][21], score[21][21];//0代表无棋子，1代表白棋，2代表黑棋
+    QString gamemode;
+    QMessageBox box;//鼠标点击处已有棋子时弹出
+    QMessageBox win;//胜利时弹出
+    QMessageBox draw;//和棋时弹出
+    QPushButton b1;
+
+
+    void initial();//初始化棋盘
+    void AImove();//AI下棋
+    void getScore();//计算棋盘上每个点的值（用于AI算法）
+    bool isEnd();//判断输赢
+    bool isDraw();//判断和棋
+
+    void paintEvent(QPaintEvent *event) override;
+    void mousePressEvent(QMouseEvent *event) override;
+    void timerEvent(QTimerEvent *event) override;
+
+public slots:
+    void slot1();
+
+signals:
+    void back();
+
+};
+
+#endif // GAMEPAGE_H
---
+++ connect6-2/mainwindow.cpp
@@ -0,0 +1,62 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QMenuBar>
+#include <QMenu>
+#include <QMessageBox>
+#include <QAction>
+#include <QPainter>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    this->setFixedSize(1200, 800);
+    this->move(350, 100);
+    QMenuBar *menubar = menuBar();
+    QMenu *menu1 = menubar->addMenu("游戏模式");
+    QMenu *menu2 = menubar->addMenu("游戏规则");
+    QAction *action1 = menu1->addAction("PvP");
+    QAction *action2 = menu1->addAction("PvE");
+    QAction *action3 = menu1->addAction("EvE");
+    QAction *action4 = menu1->addAction("client");
+    QAction *action5 = menu2->addAction("游戏规则");
+
+    QMessageBox box;//游戏规则提示窗口
+
+    connect(action1, &QAction::triggered,[=](){page.gamemode = "PvP";});
+    connect(action2, &QAction::triggered,[=](){page.gamemode = "PvE";});
+    connect(action3, &QAction::triggered,[=](){page.gamemode = "EvE";});
+    connect(action4, &QAction::triggered,[=](){page.gamemode = "client";});
+    connect(action5, &QAction::triggered,
+            [=](){
+                     QMessageBox::about(this,
+                     "游戏规则", "双方分别使用黑白两色的棋子，下在棋盘直线与横线的交叉点上，先形成六子连线者获胜。");
+                }
+            );
+
+    connect(ui->pushButton, &QPushButton::clicked, this, &MainWindow::slot1);//开始游戏按钮
+    connect(ui->pushButton_2, &QPushButton::clicked, this, &MainWindow::close);//退出游戏按钮
+    connect(&page, &gamepage::back, this, &MainWindow::slot2);//返回按钮
+
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::slot1()
+{
+    this->hide();
+    page.show();
+    page.initial();
+}
+
+void MainWindow::slot2()
+{
+    this->show();
+    page.hide();
+}
+
